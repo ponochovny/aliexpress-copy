1c122af5d4df244b3d5f6dc61194d8da4f10f406
ponochovny <ponochovny@gmail.com>
2023-07-25 10:49:22 +0300

    feat: main layout & index page & user store

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+	<MainLayout>
+		<div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+			Index Page
+		</div>
+	</MainLayout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import MainLayout from '../layouts/MainLayout.vue'
+
+export default defineComponent({
+	name: 'mainPage',
+	components: { MainLayout },
+})
+</script>
+<script lang="ts" setup></script>
