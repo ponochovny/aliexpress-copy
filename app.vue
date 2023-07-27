fbf40c662244b4dc91d2d1d0e29f7bd4a47f4fa7
ponochovny <ponochovny@gmail.com>
2023-07-28 02:25:17 +0300

    feat: menu overlay

--- app.vue
+++ app.vue
@@ -1,8 +1,50 @@
 <template>
+	<div class="fixed z-[-1] bg-[#F2F2F2] w-full h-[100vh]" />
 	<NuxtPage />
+
+	<MenuOverlay
+		:class="[
+			{
+				'max-h-[100vh] transition-all duration-200 ease-in visible':
+					userStore.isMenuOverlay,
+			},
+			{
+				'max-h-0 transition-all duration-200 ease-out invisible':
+					!userStore.isMenuOverlay,
+			},
+		]"
+	/>
 </template>
-<style>
-body {
-	background-color: #f7f7f7;
-}
-</style>
+
+<script lang="ts" setup>
+import { useUserStore } from '~/stores/user'
+
+const userStore = useUserStore()
+
+const route = useRoute()
+
+const windowWidth = ref(process.client ? window.innerWidth : '')
+
+onMounted(() => {
+	userStore.isLoading = false
+	window.addEventListener('resize', function () {
+		windowWidth.value = window.innerWidth
+	})
+})
+
+watch(
+	() => windowWidth.value,
+	() => {
+		if (typeof windowWidth.value === 'number' && windowWidth.value >= 767) {
+			userStore.isMenuOverlay = false
+		}
+	}
+)
+
+watch(
+	() => route.fullPath,
+	() => {
+		userStore.isLoading = true
+	}
+)
+</script>
