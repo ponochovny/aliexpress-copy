88c72f3557339c7ca6db5e2df44d8ab03bab1cb7
ponochovny <ponochovny@gmail.com>
2023-07-28 01:08:16 +0300

    feat: auth page mark down

---
+++ pages/auth.vue
@@ -0,0 +1,56 @@
+<template>
+	<div id="AuthPage" class="w-full h-[100vh] bg-white">
+		<div
+			class="w-full flex items-center justify-center p-5 border-b border-b-gray-300"
+		>
+			<NuxtLink to="/" class="min-w-[170px]">
+				<img src="/AliExpress-logo.png" width="170" alt="" />
+			</NuxtLink>
+		</div>
+
+		<div class="max-w-[400px] mx-auto px-2">
+			<div class="text-center my-6">Login / Register</div>
+
+			<button
+				@click="login('google')"
+				class="flex items-center justify-center gap-3 p-1.5 w-full border hover:bg-gray-100 rounded-full text-lg font-semibold"
+			>
+				<img src="/google-logo.png" alt="" class="w-full max-w-[30px]" />
+				<div>Google</div>
+			</button>
+			<button
+				@click="login('github')"
+				class="mt-4 flex items-center justify-center gap-3 p-1.5 w-full border hover:bg-gray-100 rounded-full text-lg font-semibold"
+			>
+				<img src="/github-logo.png" alt="" class="w-full max-w-[30px]" />
+				<div>Github</div>
+			</button>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { client } from 'process'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'auth',
+})
+</script>
+<script lang="ts" setup>
+// const client = useSupabaseClient()
+// const user = useSupabaseUser()
+
+// watchEffect(() => {
+// 	if (user.value) {
+// 		return navigateTo('/')
+// 	}
+// })
+
+const login = async (prov: 'google' | 'github') => {
+	alert(`Login with ${prov}`)
+	// 	const { data, error } = await client.auth.signInWithOAuth({
+	// 		provider: prov,
+	// 	})
+}
+</script>
