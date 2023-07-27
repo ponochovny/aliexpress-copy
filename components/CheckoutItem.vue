e74c366113fe24752cb682622a8a1ecce288d05c
ponochovny <ponochovny@gmail.com>
2023-07-27 12:30:53 +0300

    feat: checkout page & checkout item

---
+++ components/CheckoutItem.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="flex justify-start my-2">
+		<img :src="product.url" alt="" class="rounded-md md:w-[150px] w-[90px]" />
+
+		<div class="overflow-hidden pl-2">
+			<div class="flex items-center">
+				<span
+					class="bg-[#FD374F] text-white text-[9px] font-semibold px-1.5 rounded-sm min-w-[80px]"
+				>
+					Welcome Deal
+				</span>
+				<div class="truncate pl-2">{{ product.title }}</div>
+			</div>
+
+			<div class="text-lg font-semibold mt-2">
+				$ <span class="font-bold">{{ product.price / 100 }}</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	name: 'CheckoutItem',
+})
+</script>
+<script lang="ts" setup>
+const props = defineProps(['product'])
+const { product } = toRefs(props)
+</script>
